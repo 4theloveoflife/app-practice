33304abf51dcc941a759ba2d48356a65a0c76cbb
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-07-29 20:43:55 +0900

    Added Javascript Date
    added real time date and time

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 function formatDate(timestamp){
   let date=new Date(timestamp);
-  let hours=timestamp=date.getHours();
+  let hours=date.getHours();
+    if (hours < 10){
+    hours=`0 ${hours}`;
+  }
   let minutes=date.getMinutes();
-  let day=date.getDay();
+  if (minutes < 10){
+    minutes=`0 ${minutes}`;
+  }
+
+  let days=[
+  "Sunday", 
+  "Monday", 
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+  let day=days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
 
 
 function displayTemperature(response){
-    console.log(response.data);
      let dateElement=document.querySelector("#date");
     let temperatureElement=document.querySelector("#currentTemperature");
     let cityElement=document.querySelector("#city");
@@ -25,6 +40,5 @@ windElement.innerHTML=Math.round (response.data.wind.speed);
 
 let apiKey= "eada887ccbd6462c016357c4150a3af7";
 let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
